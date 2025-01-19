17799bc715baf3c0eb18a63d635ab2294177b612
Ajayi Bamidele <bamideleprof@gmail.com>
2025-01-19 23:51:21 +0100

    Add files via upload

---
+++ basic calculator.js
@@ -0,0 +1,51 @@
+// //  Simple calculator is done using onclick
+let display = document.querySelector(".display");
+let currentOperation = null;
+let firstOperand = null;
+
+function appendNumber(number) {
+    display.value += number ;
+}
+
+function setOperation(operation) {
+    if (display.value === "") return;
+    firstOperand = parseFloat(display.value);
+    currentOperation = operation;
+    display.value = "";
+}
+
+function calculate () {
+    if (currentOperation === null || display.value === "") return;
+    let secondOperand = parseFloat(display.value);
+    let result ;
+
+    switch (currentOperation) {
+        case "+":
+            result = firstOperand + secondOperand;
+            break;
+        case "-":
+            result = firstOperand - secondOperand
+            break;
+        case "*":
+            result = firstOperand * secondOperand;
+            break;
+        case "/":
+            result = firstOperand / secondOperand;
+            break;
+        case "%":
+            result = firstOperand % secondOperand;
+            break;
+          default:
+                return;
+    }
+
+    display.value = result;
+    currentOperation = null;
+    firstOperand = null;
+}
+
+function clearDisplay() {
+    display.value = "";
+    currentOperation = null;
+    firstOperand = null;
+}
